487f3afd0d1483dbbd1d24ac26371044a19a12eb
Eduard Karesli <ed.karesli@gmail.com>
2020-11-06 12:09:43 +0100

    Singleton in TypeScript

---
+++ main.ts
@@ -0,0 +1,24 @@
+class Singleton {
+    static _instance: Singleton;
+    data: string;
+    constructor(d: string) {
+        this.data = d;
+    }
+
+    static getInstance(d: string): Singleton {
+        if(!Singleton._instance) {
+            Singleton._instance = new Singleton(d);            
+        }
+        return Singleton._instance;
+    }
+}
+
+const theOne = Singleton.getInstance("Neo");
+
+console.log(theOne.data);
+
+const another = Singleton.getInstance("Agent Smith");
+
+console.log(another.data);
+
+console.log(theOne === another);
